93a24f32404b2b684907db5fd7402bcfa7ca63a5
Fritz Fevrier <fev17001@byui.edu>
2018-07-08 19:52:51 -0600

    add new files

--- objective2/js/script.js
+++
@@ -1,7 +0,0 @@
-// Create an object:
-var country = {name:"Brazil", continent:"South America", capiltal:"Brasilia"};
-
-
-// Display some data from the object:
-document.getElementById("demo1").innerHTML = '<b>Country Name</b>' + "" + ": " + country.name + " <br>" + '<b> Continent</b>' + ":" + country.continent + "<br>" + '<b> Capital</b>' + ":" + country.capiltal;
-
